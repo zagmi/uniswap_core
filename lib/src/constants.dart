438669ddc7c25773fcc2d9a65c73055c9fc1e71c
Dalia Mora <dalia63ramirez@gmail.com>
2023-07-28 21:52:59 +0000

    Added @zagmi dart package

---
+++ lib/src/constants.dart
@@ -0,0 +1,3 @@
+final maxUint256 = BigInt.parse('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', radix: 16);
+
+final initCodeHash = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
--- lib/src/utils/computePairAddress.dart
+++ lib/src/utils/computePairAddress.dart
@@ -1,10 +1,19 @@
-EthereumAddress computePairAddress(EthereumAddress factoryAddress, Token tokenA, Token tokenB) {
+import 'dart:typed_data' show Uint8List;
+import 'package:zagmi/crypto.dart';
+import 'package:zagmi/zagmi.dart';
+import '../constants.dart';
+
+EthereumAddress computePairAddress(
+    EthereumAddress factoryAddress, Token tokenA, Token tokenB) {
   var tokens = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA];
 
   var startBytes = hexToBytes('0xff');
   var factoryBytes = hexToBytes(factoryAddress.hex);
-  var saltBytes = keccak256(hexToBytes(tokens[0].address.hexNo0x + tokens[1].address.hexNo0x));
-  var initCodeHashBytes = hexToBytes(INIT_CODE_HASH);
+  var saltBytes = keccak256(
+      hexToBytes(tokens[0].address.hexNo0x + tokens[1].address.hexNo0x));
+  var initCodeHashBytes = hexToBytes(initCodeHash);
 
-  return EthereumAddress(keccak256(Uint8List.fromList(startBytes + factoryBytes + saltBytes + initCodeHashBytes)).sublist(12));
+  return EthereumAddress(keccak256(Uint8List.fromList(
+          startBytes + factoryBytes + saltBytes + initCodeHashBytes))
+      .sublist(12));
 }
