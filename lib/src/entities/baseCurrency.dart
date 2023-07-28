16043e9dc358bf62267cc5408013309853d36227
Dalia Mora <dalia63ramirez@gmail.com>
2023-07-28 22:25:46 +0000

    baseCurrency

--- lib/src/entities/baseCurrency.dart
+++ lib/src/entities/baseCurrency.dart
@@ -9,21 +9,37 @@ abstract class BaseCurrency {
   late final bool isToken;
 
   /// The chain ID on which this currency resides
-  late final bool chainId;
+  late final num chainId;
 
   /// The decimals used in representing currency amounts
-  late final bool decimals;
+  late final num decimals;
 
   /// The symbol of the currency, i.e. a short textual non-unique identifier
-  late final String symbol;
+  late final String? symbol;
 
   /// The name of the currency, i.e. a descriptive textual non-unique identifier
-  late final String namel;
+  late final String? name;
 
   /// Constructs an instance of the base class `BaseCurrency`-
   /// `chainId` the chain ID on which this currency resides
   /// `decimals` of the currency
   /// `symbol` of the currency
   /// `name` of the currency
-  BaseCurrency() {}
+  BaseCurrency(num chainId, num decimals, String? symbol, String? name) {
+    invariant(chainId is int, 'CHAIN_ID');
+    invariant(decimals >= 0 && decimals < 255 && decimals is int, 'DECIMALS');
+
+    this.chainId = chainId;
+    this.decimals = decimals;
+    this.symbol = symbol;
+    this.name = name;
+  }
+
+  /// Returns whether this currency is functionally equivalent to the other currency
+  /// `other` the other currency
+  bool equals(Currency other);
+
+  /// Return the wrapped version of this currency that can be used with the Uniswap contracts. Currencies must
+  /// implement this to be used in Uniswap 
+  Token get wrapped;
 }
