f0965ffecb6eb1283322155008cef27fa6727786
Dalia Mora <dalia63ramirez@gmail.com>
2023-07-29 00:13:19 +0000

    Fixing...

--- lib/src/utils/sqrt.dart
+++ lib/src/utils/sqrt.dart
@@ -1,7 +1,7 @@
-import 'dart:ffi' as ffi;
+import 'dart:math' as math;
 import 'package:tiny_invariant/tiny_invariant.dart';
 
-final maxSafeInteger = ffi.Int64. >> 1;
+final maxSafeInteger = BigInt.from(math.pow(2, 53)) - BigInt.one;
 
 final zero = BigInt.from(0);
 final one = BigInt.from(1);
@@ -10,21 +10,20 @@ final two = BigInt.from(2);
 /// Computes floor(sqrt(value))
  /// `value` the value for which to compute the square root, rounded down
 
-BigInt sqrt(num value) {
-  invariant(value  >=zero.toInt(), "NEGATIVE");
-
+BigInt sqrt(BigInt value) {
+  invariant(value  >=zero, "NEGATIVE");
   // rely on built in sqrt if possible
-  if (value < maxSafeInteger.toInt()) {
+  if (value < maxSafeInteger) {
     return BigInt.from(Math.floor(Math.sqrt(JSBI.toNumber(value))))
   }
 
-  let z: JSBI
-  let x: JSBI
-  z = value
-  x = JSBI.add(JSBI.divide(value, TWO), ONE)
-  while (JSBI.lessThan(x, z)) {
-    z = x
-    x = JSBI.divide(JSBI.add(JSBI.divide(value, x), x), TWO)
+  BigInt z;
+  BigInt x;
+  z = value;
+  x = (BigInt.from(value) / two) + one;
+  while (x < z) {
+    z = x;
+    x = JSBI.divide(JSBI.add(JSBI.divide(value, x), x), TWO);
   }
   return z;
 }
